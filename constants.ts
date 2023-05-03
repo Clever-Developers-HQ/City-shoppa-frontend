159b846108707f396426fe18e5c7b7f98982cadf
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-05-03 15:26:10 -0500

    update

--- constants.ts
+++ constants.ts
@@ -1,9 +1,8 @@
-// constants.ts
-// const API_BASEURL =
-//   process.env.NODE_ENV === "production"
-//     ? process.env.NEXT_PUBLIC_API_URL_PROD
-//     : process.env.NEXT_PUBLIC_API_URL_DEV;
+const API_BASEURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_API_URL_PROD
+    : process.env.NEXT_PUBLIC_API_URL_DEV;
 
-const API_BASEURL = 'https://cityshoppa.onrender.com/api/v1'
+// const API_BASEURL = 'https://cityshoppa.onrender.com/api/v1'
 
 export default API_BASEURL;
