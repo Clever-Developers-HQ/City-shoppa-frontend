0028d322c3a6b9acca9101dbdae9987ef648144a
Boyinbode Ebenezer Ayomide <enegxi1@gmail.com>
2023-05-04 05:57:30 -0500

    mm

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     // this includes files from the monorepo base two directories up
-    outputFileTracingRoot: path.join(__dirname, '../../'),
+    outputFileTracingRoot: path.join(__dirname, '../'),
   },
   images: {
     // loaderFile: './src/components/loader/Loader.tsx',
